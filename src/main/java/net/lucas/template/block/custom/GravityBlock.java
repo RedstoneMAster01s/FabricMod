84495328bc1e1a4be6ce98f4f15c107dc903035c
casno <redstonemasterlukas109@gmail.com>
2024-07-04 16:21:59 -0400

    Hi

---
+++ src/main/java/net/lucas/template/block/custom/GravityBlock.java
@@ -0,0 +1,24 @@
+package net.lucas.template.block.custom;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public class GravityBlock extends Block {
+
+    public GravityBlock(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public void onSteppedOn(World world, BlockPos pos, BlockState state, Entity entity) {
+        if (!world.isClient && entity instanceof PlayerEntity) {
+            PlayerEntity player = (PlayerEntity) entity;
+            player.addVelocity(0, 0.5, 0); // Adjust the Y velocity for reduced gravity effect
+        }
+        super.onSteppedOn(world, pos, state, entity);
+    }
+}
