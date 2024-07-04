b25363140c82e05ed06cd5766ffe4584608dba93
casno <redstonemasterlukas109@gmail.com>
2024-07-04 16:22:31 -0400

    Hi

--- src/main/java/net/lucas/template/block/custom/GravityBlock.java
+++ src/main/java/net/lucas/template/block/custom/GravityBlock.java
@@ -7,7 +7,6 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
-
 public class GravityBlock extends Block {
 
     public GravityBlock(Settings settings) {
